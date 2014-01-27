8f65f50abbdb8031915a29b54d90e376cee132ea
Wout <wout@woutmager.nl>
2014-01-27 16:53:59 +0100

    ..

--- Gruntfile.js
+++ Gruntfile.js
@@ -95,7 +95,7 @@ module.exports = function(grunt) {
         
             // When parseFiles = true, this task will crawl all *.js, *.css, *.scss files, except files that are in node_modules/.
             // You can override this by defining a 'files' array below.
-            // 'files' : [],
+            'files' : ['./style.css'],
         
             // When parseFiles = true, matchCommunityTests = true will attempt to
             // match user-contributed tests.
