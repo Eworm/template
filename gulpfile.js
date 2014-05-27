94f1e74b4d1b1090ffbec00accf86fc3b14e2370
Wout <wout@woutmager.nl>
2014-05-27 11:10:02 +0200

    Cleaner gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,8 @@ lr = require('tiny-lr'),
 server = lr();
 
 
-// Set standard paths
-var paths = {
+// Set standard src_paths
+var src_paths = {
     compass: './sass/**/*.scss',
     autoprefixer: '*.css',
     svgmin: './img-src/*.svg',
@@ -26,21 +26,28 @@ var paths = {
                 './js-src/yepnope-loader.js']
 };
 
+var dest_paths = {
+    compass: '.',
+    svgmin: './img',
+    svg2png: './img',
+    imagemin: './img',
+    uglify: './js',
+    yepnope: './js'
+};
+
 
 // Compass
 gulp.task('compass', function() {
-    gulp.src(paths.compass)
+    gulp.src(src_paths.compass)
     
         .pipe(plugins.plumber({errorHandler: plugins.notify.onError("Error: <%= error.message %>")}))
         
         .pipe(plugins.compass({
             config_file: './config.rb',
             project: '.',
-            css: '.',
+            css: dest_paths.compass,
             image: 'img',
             font: 'fonts',
-            //style: 'compressed',
-            //comments: false,
             import_path: 'bower_components',
             sourcemap: true
         }))
@@ -55,9 +62,9 @@ gulp.task('compass', function() {
 
 // SVG optim
 gulp.task('svgmin', function() {
-    gulp.src(paths.svgmin)
+    gulp.src(src_paths.svgmin)
         .pipe(plugins.svgmin())
-        .pipe(gulp.dest('./img'))
+        .pipe(gulp.dest(dest_paths.svgmin))
         
         .pipe(plugins.livereload(server))
         .pipe(plugins.notify({ message: 'Svgoptim complete' }))
@@ -66,9 +73,9 @@ gulp.task('svgmin', function() {
 
 // SVG 2 png
 gulp.task('svg2png', function () {
-    gulp.src(paths.svg2png)
+    gulp.src(src_paths.svg2png)
         .pipe(plugins.svg2png())
-        .pipe(gulp.dest('./img'))
+        .pipe(gulp.dest(dest_paths.svg2png))
         
         .pipe(plugins.livereload(server))
         .pipe(plugins.notify({ message: 'Svg2png complete' }))
@@ -77,9 +84,9 @@ gulp.task('svg2png', function () {
 
 // Imagemin - run just before uploading
 gulp.task('imagemin', function () {
-    gulp.src(paths.imagemin)
+    gulp.src(src_paths.imagemin)
         .pipe(plugins.imagemin())
-        .pipe(gulp.dest('./img'))
+        .pipe(gulp.dest(dest_paths.imagemin))
         .pipe(plugins.notify({ message: 'Imagemin complete' }))
 });
 
@@ -87,7 +94,7 @@ gulp.task('imagemin', function () {
 // Uglify
 gulp.task('uglify', function() {
     
-    gulp.src(paths.uglify)
+    gulp.src(src_paths.uglify)
     
         .pipe(plugins.plumber({errorHandler: plugins.notify.onError("Error: <%= error.message %>")}))
     
@@ -95,19 +102,19 @@ gulp.task('uglify', function() {
         // Strip console and debugger statements from JavaScript code
         .pipe(plugins.stripDebug())
         .pipe(plugins.uglify())
-        .pipe(gulp.dest('./js'))
+        .pipe(gulp.dest(dest_paths.uglify))
         
         .pipe(plugins.livereload(server))
         .pipe(plugins.notify({ message: 'Uglify complete' }))
         
-    gulp.src(paths.yepnope)
+    gulp.src(src_paths.yepnope)
     
         .pipe(plugins.plumber({errorHandler: plugins.notify.onError("Error: <%= error.message %>")}))
     
         .pipe(plugins.concat('yepnope.min.js'))
         .pipe(plugins.stripDebug())
         .pipe(plugins.uglify())
-        .pipe(gulp.dest('./js'))
+        .pipe(gulp.dest(dest_paths.uglify))
         
         .pipe(plugins.livereload(server))
         .pipe(plugins.notify({ message: 'Uglify complete' }))
@@ -116,7 +123,7 @@ gulp.task('uglify', function() {
 
 // SCSS lint
 gulp.task('lint', function() {
-    gulp.src('./sass/**/*.scss')
+    gulp.src(src_paths.compass)
         .pipe(plugins.scsslint())
         .pipe(plugins.scsslint.reporter());
 });
@@ -125,10 +132,10 @@ gulp.task('lint', function() {
 // Watch
 gulp.task('watch', function() {
     server.listen(35729, function(err) {
-    	gulp.watch(paths.compass, ['compass']);
-    	gulp.watch([paths.uglify, paths.yepnope], ['uglify']);
-    	gulp.watch(paths.svgmin, ['svgmin']);
-    	gulp.watch(paths.svg2png, ['svg2png']);
+    	gulp.watch(src_paths.compass, ['compass']);
+    	gulp.watch([src_paths.uglify, src_paths.yepnope], ['uglify']);
+    	gulp.watch(src_paths.svgmin, ['svgmin']);
+    	gulp.watch(src_paths.svg2png, ['svg2png']);
     })
 });
 
