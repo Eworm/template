637674183696688d7a7fbed8199a8a7d1985a585
Wout <wout@woutmager.nl>
2014-07-03 14:36:37 +0200

    Added a maximum scale

--- header.php
+++ header.php
@@ -79,7 +79,7 @@
         <meta name="msapplication-TileColor" content="#000">
         
         <!-- Mobile viewport optimized: h5bp.com/viewport -->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
         
         <!-- Microsoft. Delete if not required -->
         <meta http-equiv="cleartype" content="on">
