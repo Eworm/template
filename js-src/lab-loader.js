28f907ea18408f38b5cc473069dee16285bb7799
Wout <wout@woutmager.nl>
2014-09-15 20:36:14 +0200

    ..

--- js-src/lab-loader.js
+++ js-src/lab-loader.js
@@ -3,6 +3,7 @@ var $L = $LAB
 .script('https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js').wait()
 .script(templateUrl + '/js/functions.min.js');
 
+// Add google maps only when map_address exists
 if (typeof map_address != 'undefined') { $L = $L
 .script('http://maps.googleapis.com/maps/api/js?key=AIzaSyBgU-OMTQXQwUwOyNRZB5jh1cFgY5z_L2A&sensor=false&callback=initialize_single').wait();
 }
