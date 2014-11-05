59407ba4252835fe0cb3cba889babab0ddb81493
Wout <wout@woutmager.nl>
2014-11-05 13:13:08 +0100

    ..

--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
     <fieldset class="searchform-container">
 
         <input type="search" value="" name="s" id="s" placeholder="<?php _e( 'Zoeken in de site', 'thema_vertalingen' ); ?>">
-        <input type="submit" id="searchsubmit" value="<?php _e( 'Zoek', 'thema_vertalingen' ); ?>" class="submit">
+        <input type="submit" value="<?php _e( 'Zoek', 'thema_vertalingen' ); ?>" class="submit searchsubmit">
 
     </fieldset>
 
