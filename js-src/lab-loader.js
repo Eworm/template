7b1e3d608239d1583c12bfb7b937595db5e53096
Wout <wout@woutmager.nl>
2015-07-30 12:45:43 +0200

    ..

--- js-src/lab-loader.js
+++ js-src/lab-loader.js
@@ -1,5 +1,5 @@
 var $L = $LAB
-.script(templateUrl + '/js/modernizr.min.js').wait()
+// .script(templateUrl + '/js/modernizr.min.js').wait()
 .script('https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js').wait()
 .script(templateUrl + '/js/functions.min.js');
 
