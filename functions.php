c642036a92357013145adc8a5424afe23ebe7d81
Wout <wout@woutmager.nl>
2016-03-01 17:58:22 +0100

    Remove ugly wp stuff

--- functions.php
+++ functions.php
@@ -33,6 +33,10 @@ add_action('template_redirect', 'load_jquery');
 remove_filter('term_description','wpautop');
 remove_action('wp_head', 'wp_generator');
 remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('admin_print_styles', 'print_emoji_styles');
 
 
 /* Widget
