d4376f3898c4b57697b8bd2854e25e373aa7330d
Wout <wout@woutmager.nl>
2016-03-29 11:10:13 +0200

    Removed the WP embed script

--- functions.php
+++ functions.php
@@ -38,6 +38,23 @@ remove_action('admin_print_scripts', 'print_emoji_detection_script');
 remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action('admin_print_styles', 'print_emoji_styles');
 
+function disable_embeds_init() {
+
+    // Remove the REST API endpoint.
+    remove_action('rest_api_init', 'wp_oembed_register_route');
+
+    // Turn off oEmbed auto discovery.
+    // Don't filter oEmbed results.
+    remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
+
+    // Remove oEmbed discovery links.
+    remove_action('wp_head', 'wp_oembed_add_discovery_links');
+
+    // Remove oEmbed-specific JavaScript from the front-end and back-end.
+    remove_action('wp_head', 'wp_oembed_add_host_js');
+}
+add_action('init', 'disable_embeds_init', 9999);
+
 
 /* Widget
 ==================================================================================================================================*/
