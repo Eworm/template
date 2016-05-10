5b19b340158fb767b9819e64830aaec3d1d68a7c
Wout <wout@woutmager.nl>
2016-05-10 15:01:48 +0200

    ..

--- header.php
+++ header.php
@@ -12,18 +12,6 @@
             global $page, $paged;
         
             wp_title( '|', true, 'right' );
-        
-            // Add the blog name.
-            bloginfo( 'name' );
-        
-            // Add the blog description for the home/front page.
-            $site_description = get_bloginfo( 'description', 'display' );
-            if ( $site_description && ( is_home() || is_front_page() ) )
-                echo " | $site_description";
-        
-            // Add a page number if necessary:
-            if ( $paged >= 2 || $page >= 2 )
-                echo ' | ' . sprintf( __( 'Pagina %s', 'thema_vertalingen' ), max( $paged, $page ) );
         ?>
         </title>
         
