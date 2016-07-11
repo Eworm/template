baf2f5caa7c7ab68191afb25ab14e956e4318a71
Wout <wout@woutmager.nl>
2016-07-11 13:45:46 +0200

    ..

--- page.php
+++ page.php
@@ -58,39 +58,6 @@
                         
                     </div>
                     
-<!--
-                    <div id="thumbs">
-                    
-                        <?php
-                            $args = array(
-                                'order'          => 'ASC',
-                                'orderby'       => 'menu_order',
-                            	'post_type'   => 'attachment',
-                                'numberposts' => -1,
-                                'post_status' => null,
-                                'post_parent' => $post->ID,
-                                'exclude'     => get_post_thumbnail_id()
-                                );
-                                
-                            $attachments = get_posts( $args );
-                            
-                            if ( $attachments ) :
-                            
-                                foreach ( $attachments as $attachment ) {
-                                    $image_attributes = wp_get_attachment_image_src( $attachment->ID, 'large' );
-                                    echo '<a class="fancybox" rel="gallery" href="';
-                                    echo $image_attributes[0];
-                                    echo '">';
-                                    echo wp_get_attachment_image( $attachment->ID ); 
-                                    echo '</a>';
-                                }
-                            
-                            endif;
-                        ?>
-                        
-                    </div>
--->
-    
                 </main> <!-- .page-content -->
     
             <?php endwhile; ?>
