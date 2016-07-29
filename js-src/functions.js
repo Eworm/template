34b4e6f39eef633b6270d090f0523b2b7b3525df
Wout <wout@woutmager.nl>
2016-07-29 21:15:32 +0200

    ..

--- js-src/functions.js
+++ js-src/functions.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-        
-    
-    
+
+
+
     // Lazy loading: add b-lazy class to the image
     var bLazy = new Blazy();
-    
-    
-    
+
+
+
     // Animation class
     setTimeout(function()
     {
@@ -23,7 +23,7 @@ $(document).ready(function() {
     });
 */
     // Toggle password
-    $('#show-password').on('change', function () 
+    $('#show-password').on('change', function ()
     {
         // When the '#show-password' checkbox changes its value,
         // set the visibility of the password field to whatever
@@ -37,113 +37,123 @@ $(document).ready(function() {
 
     // Commentform validation
     $('#commentform').parsley();
-    
-        
-    
+
+
+
     // Palm media queries
     var handleMatchMedia = function (mediaQuery)
     {
-        if (mediaQuery.matches) {
+        if (mediaQuery.matches)
+        {
 
             // Load mobile stuff
             console.log('Below 480px');
 
-        } else {
-            
+        } else
+        {
+
             //load desktop stuff
             console.log('Above 480px');
         }
     },
     mqlPalm = window.matchMedia('all and (max-width: 30em)');
-    
+
     handleMatchMedia(mqlPalm);                  //Execute on load
     mqlPalm.addListener(handleMatchMedia);      //Execute each time media query will be reached
-    
-    
-    
-    
+
+
+
+
     // Lap media queries
     var handleMatchMedia = function (mediaQuery)
     {
-        if (mediaQuery.matches) {
+        if (mediaQuery.matches)
+        {
 
             // Load mobile stuff
             console.log('Below 750px');
 
-        } else {
-            
+        } else
+        {
+
             //load desktop stuff
             console.log('Above 750px');
         }
     },
     mqlLap = window.matchMedia('all and (max-width: 46.875em)');
-    
+
     handleMatchMedia(mqlLap);                  //Execute on load
     mqlLap.addListener(handleMatchMedia);      //Execute each time media query will be reached
-    
-    
-    
-    
+
+
+
+
     // Desk media queries
     var handleMatchMedia = function (mediaQuery)
     {
-        if (mediaQuery.matches) {
+        if (mediaQuery.matches)
+        {
 
             // Load mobile stuff
             console.log('Below 970px');
 
-        } else {
-            
+        } else
+        {
+
             //load desktop stuff
             console.log('Above 970px');
         }
     },
     mqlDesk = window.matchMedia('all and (max-width: 60.625em)');
-    
+
     handleMatchMedia(mqlDesk);                  //Execute on load
     mqlDesk.addListener(handleMatchMedia);      //Execute each time media query will be reached
-    
-    
-    
-    
+
+
+
+
     // Wall media queries
     var handleMatchMedia = function (mediaQuery)
     {
-        if (mediaQuery.matches) {
+        if (mediaQuery.matches)
+        {
 
             // Load mobile stuff
             console.log('Below 1170px');
 
-        } else {
-            
+        } else
+        {
+
             //load desktop stuff
             console.log('Above 1170px');
         }
     },
     mqlWall = window.matchMedia('all and (max-width: 73.125em)');
-    
+
     handleMatchMedia(mqlWall);                  //Execute on load
     mqlWall.addListener(handleMatchMedia);      //Execute each time media query will be reached
-    
-    
-    
-    
+
+
+
+
     // Cinema media queries
     var handleMatchMedia = function (mediaQuery)
     {
-        if (mediaQuery.matches) {
+        if (mediaQuery.matches)
+        {
 
             // Load mobile stuff
             console.log('Below 1430px');
 
-        } else {
-            
+        } else
+        {
+
             //load desktop stuff
             console.log('Above 1430px');
         }
     },
     mqlCinema = window.matchMedia('all and (max-width: 89.375em)');
-    
+
     handleMatchMedia(mqlCinema);                  //Execute on load
     mqlCinema.addListener(handleMatchMedia);      //Execute each time media query will be reached
 
@@ -173,14 +183,14 @@ function codeLocations_single(list, map)
     for (var i = 0; i < list.length; i++)
     {
         list = window[list] || list;
-        
+
         var addressId = list[i].id;
         var geocoder = new google.maps.Geocoder();
         var geoOptions = {
             address: list[i].location,
             region: 'NO'
         };
-        
+
         geocoder.geocode(geoOptions, createGeocodeCallback_single(list[i], map, addressId));
     }
 }
@@ -195,7 +205,7 @@ function createGeocodeCallback_single(item, map, addressId)
         {
             addMarker_single(map, item, results[0].geometry.location, addressId);
         } else {
-            
+
         }
     }
 }
@@ -210,15 +220,15 @@ function addMarker_single(map, item, location, addressId)
         map: map,
         position: location
     })
-    
+
     var bounds = new google.maps.LatLngBounds();
     bounds.extend(location);
     map.fitBounds(bounds);
-    
+
     zoomChangeBoundsListener =
     google.maps.event.addListenerOnce(map, 'bounds_changed', function(event)
     {
-        if (this.getZoom()) 
+        if (this.getZoom())
         {
             this.setZoom(14);
         }
