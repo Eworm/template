db1e0d1c718d6d34d729ef373cce90f8050be113
Wout <wout@woutmager.nl>
2016-09-22 14:35:46 +0200

    ..

--- includes/wp-imagesizes.php
+++ includes/wp-imagesizes.php
@@ -1,8 +1,10 @@
 <?php
 
 /* New images sizes
+   More info: https://developer.wordpress.org/reference/functions/add_image_size/
 ==================================================================================================================================*/
-if ( function_exists( 'add_image_size' ) ) {
+if ( function_exists( 'add_image_size' ) )
+{
     //add_image_size( 'home-size', 500, 500, array( 'left', 'top' ) ); // Hard crop left top
 }
 
