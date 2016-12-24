6f8fd229bc4c85a93a325bc6f28828f94d0bca7c
Wout <wout@woutmager.nl>
2016-12-24 12:50:46 +0100

    Better includes

---
+++ includes/wm-breadcrumbs.php
@@ -0,0 +1,90 @@
+<?php
+
+/* Breadcrumbs without a plugin
+==================================================================================================================================*/
+function the_breadcrumb()
+{
+    global $post;
+    echo '<ul class="breadcrumbs">';
+
+    if (!is_home())
+    {
+        echo '<li><a href="';
+        echo get_option('home');
+        echo '" title="Naar de homepage">';
+        echo 'Home';
+        echo '</a></li>';
+
+        if (is_category() || is_single())
+        {
+            echo '<li>';
+            the_category('</li><li>');
+            if (is_single())
+            {
+                echo '</li><li><span>';
+                the_title();
+                echo '<span></li>';
+            }
+
+        }
+
+        elseif (is_page())
+        {
+            if($post->post_parent)
+            {
+                $anc = get_post_ancestors( $post->ID );
+                $title = get_the_title();
+                foreach ( $anc as $ancestor )
+                {
+                    $output = '<li><a href="'.get_permalink($ancestor).'" title="'.get_the_title($ancestor).'">'.get_the_title($ancestor).'</a></li>';
+                }
+                echo $output;
+                // echo '<li><span>';
+                // echo $title;
+                // echo '</span></li>';
+            }
+            else
+            {
+                // echo '<li><span>' . get_the_title() . '</span></li>';
+            }
+        }
+    }
+    elseif (is_tag())
+    {
+        single_tag_title();
+    }
+
+    elseif (is_day())
+    {
+        echo"<li>Archive for "; the_time('F jS, Y'); echo'</li>';
+    }
+
+    elseif (is_month())
+    {
+        echo"<li>Archive for "; the_time('F, Y'); echo'</li>';
+    }
+
+    elseif (is_year())
+    {
+        echo"<li>Archive for "; the_time('Y'); echo'</li>';
+    }
+
+    elseif (is_author())
+    {
+        echo"<li>Author Archive"; echo'</li>';
+    }
+
+    elseif (isset($_GET['paged']) && !empty($_GET['paged']))
+    {
+        echo "<li>Blog Archives"; echo'</li>';
+    }
+
+    elseif (is_search())
+    {
+        echo"<li>Search Results"; echo'</li>';
+    }
+
+    echo '</ul>';
+}
+
+?>
---
+++ includes/wm-menu.php
@@ -0,0 +1,15 @@
+<?php
+
+/* Register menu
+==================================================================================================================================*/
+function register_my_menus()
+{
+    register_nav_menus(
+        array(
+            'main-menu' => 'Hoofdmenu'
+        )
+    );
+}
+add_action( 'init', 'register_my_menus' );
+
+?>
