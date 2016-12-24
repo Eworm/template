6a90e4dc96c1407efabf37abcd5354c107056278
Wout <wout@woutmager.nl>
2016-12-24 12:52:26 +0100

    ..

--- functions.php
+++ functions.php
@@ -3,15 +3,13 @@
 
 /* Includes
 ==================================================================================================================================*/
-include_once( get_stylesheet_directory() . '/includes/clean.php');
-// include_once( get_stylesheet_directory() . '/includes/menu.php');
-include_once( get_stylesheet_directory() . '/includes/minify.php');
-include_once( get_stylesheet_directory() . '/includes/breadcrumbs.php');
 include_once( get_stylesheet_directory() . '/includes/required-plugins.php');
-include_once( get_stylesheet_directory() . '/includes/contact.php');
+include_once( get_stylesheet_directory() . '/includes/wm-minify.php');
+include_once( get_stylesheet_directory() . '/includes/wm-breadcrumbs.php');
+include_once( get_stylesheet_directory() . '/includes/wm-contact.php');
+include_once( get_stylesheet_directory() . '/includes/wp-clean.php');
 include_once( get_stylesheet_directory() . '/includes/wp-widgets.php');
 include_once( get_stylesheet_directory() . '/includes/wp-options.php');
-// include_once( get_stylesheet_directory() . '/includes/wp-imagesizes.php');
 include_once( get_stylesheet_directory() . '/includes/wp-submenu-walker.php');
 include_once( get_stylesheet_directory() . '/includes/wp-custom-post-types.php');
 include_once( get_stylesheet_directory() . '/includes/acf-options.php');
