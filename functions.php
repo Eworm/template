5131367d9854597f0c8739743c6214335074b185
Wout <wout@woutmager.nl>
2017-04-25 09:31:37 +0200

    ..

--- functions.php
+++ functions.php
@@ -10,7 +10,8 @@ include_once( get_stylesheet_directory() . '/includes/wm-contact.php');
 include_once( get_stylesheet_directory() . '/includes/wp-clean.php');
 include_once( get_stylesheet_directory() . '/includes/wp-widgets.php');
 include_once( get_stylesheet_directory() . '/includes/wp-options.php');
-include_once( get_stylesheet_directory() . '/includes/wp-custom-post-types.php');
+include_once( get_stylesheet_directory() . '/includes/wp-custom-post-type.php');
+include_once( get_stylesheet_directory() . '/includes/wp-custom-post-type-icons.php');
 include_once( get_stylesheet_directory() . '/includes/acf-options.php');
 include_once( get_stylesheet_directory() . '/includes/plugin-timber.php');
 
---
+++ includes/wp-custom-post-type-icons.php
@@ -0,0 +1,25 @@
+<?php
+    
+    
+/* Custom post type icons
+   Get the icon content here: https://developer.wordpress.org/resource/dashicons/#welcome-write-blog
+==================================================================================================================================*/
+function add_menu_icons_styles()
+{
+?>
+
+<style>
+#adminmenu .menu-icon-men div.wp-menu-image:before {
+    content: "\f110";
+}
+#adminmenu .menu-icon-women div.wp-menu-image:before {
+    content: "\f110";
+}
+</style>
+
+<?php
+}
+add_action( 'admin_head', 'add_menu_icons_styles' );
+
+
+?>
