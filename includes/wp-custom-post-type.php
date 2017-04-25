2cccf1cbeaf4ad7e94135a7517ddb9a5a7aa3955
Wout <wout@woutmager.nl>
2017-04-25 10:30:56 +0200

    ..

--- includes/wp-custom-post-type.php
+++ includes/wp-custom-post-type.php
@@ -75,21 +75,4 @@ function create_cpt_taxonomies()
 add_action( 'init', 'create_cpt_taxonomies', 0 );
 
 
-/* Custom post type icons
-   Get the icon content here: https://developer.wordpress.org/resource/dashicons/#welcome-write-blog
-==================================================================================================================================*/
-function add_menu_icons_styles()
-{
-?>
-
-<style>
-#adminmenu .menu-icon-mediatheek div.wp-menu-image:before {
-    content: "\f110";
-}
-</style>
-
-<?php
-}
-add_action( 'admin_head', 'add_menu_icons_styles' );
-
 ?>
