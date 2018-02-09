f0fa5e4ef8d60c7eaff4556640ee2d9899016950
Wout <wout@woutmager.nl>
2018-02-09 09:39:32 +0100

    Disabling sidebar

--- includes/wp-widgets.php
+++ includes/wp-widgets.php
@@ -2,11 +2,11 @@
 
 /* Widget
 ==================================================================================================================================*/
-if (function_exists('register_sidebar')) {
-    register_sidebar(array('name'=>'Widget',
-    'before_widget' => '<section class="sidebar__section">',
-    'after_widget' => '</section>',
-    'before_title' => '<h1 class="sidebar__section__header">',
-    'after_title' => '</h1>',
-));
-}
+// if (function_exists('register_sidebar')) {
+//     register_sidebar(array('name'=>'Widget',
+//     'before_widget' => '<section class="sidebar__section">',
+//     'after_widget' => '</section>',
+//     'before_title' => '<h1 class="sidebar__section__header">',
+//     'after_title' => '</h1>',
+// ));
+// }
