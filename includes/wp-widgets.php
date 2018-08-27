d82962aff5ec34f285dc3bd892f8dff67682a2fa
Wout <wout@woutmager.nl>
2018-08-27 20:11:31 +0200

    Adding some changes and helpfull stuff which I learned through new projects

--- includes/wp-widgets.php
+++ includes/wp-widgets.php
@@ -4,9 +4,9 @@
 ==================================================================================================================================*/
 // if (function_exists('register_sidebar')) {
 //     register_sidebar(array('name'=>'Widget',
-//     'before_widget' => '<section class="sidebar__section">',
+//     'before_widget' => '<section class="aside__section">',
 //     'after_widget' => '</section>',
-//     'before_title' => '<h1 class="sidebar__section__header">',
+//     'before_title' => '<h1 class="aside__section__header">',
 //     'after_title' => '</h1>',
 // ));
 // }
