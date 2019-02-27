b381f913cec55e66345d11c4d05e39eeefe50a26
Wout <wout@woutmager.nl>
2019-02-27 23:13:50 +0100

    ..

--- functions/wp-hide-admin-items.php
+++ functions/wp-hide-admin-items.php
@@ -2,17 +2,17 @@
 
 /*  Remove uncommmented admin pages
 ==================================================================================================================================*/
-function remove_menus(){
-    // remove_menu_page( 'index.php' );                  // Dashboard
-    // remove_menu_page( 'jetpack' );                    // Jetpack*
-    // remove_menu_page( 'edit.php' );                   // Posts
-    // remove_menu_page( 'upload.php' );                 // Media
-    // remove_menu_page( 'edit.php?post_type=page' );    // Pages
-    // remove_menu_page( 'edit-comments.php' );          // Comments
-    // remove_menu_page( 'themes.php' );                 // Appearance
-    // remove_menu_page( 'plugins.php' );                // Plugins
-    // remove_menu_page( 'users.php' );                  // Users
-    // remove_menu_page( 'tools.php' );                  // Tools
-    // remove_menu_page( 'options-general.php' );        // Settings
+function remove_menus() {
+    remove_menu_page('index.php');                  // Hide Dashboard
+    remove_menu_page('jetpack');                    // Hide Jetpack
+    remove_menu_page('edit.php');                   // Hide Posts
+    remove_menu_page('upload.php');                 // Hide Media
+    remove_menu_page('edit.php?post_type=page');    // Hide Pages
+    remove_menu_page('edit-comments.php');          // Hide Comments
+    remove_menu_page('themes.php');                 // Hide Appearance
+    remove_menu_page('plugins.php');                // Hide Plugins
+    remove_menu_page('users.php');                  // Hide Users
+    remove_menu_page('tools.php');                  // Hide Tools
+    remove_menu_page('options-general.php');        // Hide Settings
 }
-add_action( 'admin_menu', 'remove_menus' );
+// add_action( 'admin_menu', 'remove_menus');
