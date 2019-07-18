d17d429502c104b3dc0f213aa0e3d1ab981d9d55
Wout <wout@woutmager.nl>
2019-07-18 19:44:28 +0200

    ..

--- functions.php
+++ functions.php
@@ -2,20 +2,20 @@
 
 /* Includes
 ==================================================================================================================================*/
-include_once(get_stylesheet_directory() . '/functions/required-plugins.php');
-include_once(get_stylesheet_directory() . '/functions/wm-minify.php');
-include_once(get_stylesheet_directory() . '/functions/wm-breadcrumbs.php');
-include_once(get_stylesheet_directory() . '/functions/wm-contact.php');
-include_once(get_stylesheet_directory() . '/functions/wm-autocomplete.php');
-include_once(get_stylesheet_directory() . '/functions/wp-clean.php');
-include_once(get_stylesheet_directory() . '/functions/wp-widgets.php');
-include_once(get_stylesheet_directory() . '/functions/wp-options.php');
-include_once(get_stylesheet_directory() . '/functions/wp-custom-post-type.php');
-include_once(get_stylesheet_directory() . '/functions/wp-custom-post-type-icons.php');
-include_once(get_stylesheet_directory() . '/functions/wp-image-sizes.php');
-include_once(get_stylesheet_directory() . '/functions/wp-hide-admin-items.php');
 include_once(get_stylesheet_directory() . '/functions/acf-options.php');
 include_once(get_stylesheet_directory() . '/functions/plugin-timber.php');
+include_once(get_stylesheet_directory() . '/functions/required-plugins.php');
+include_once(get_stylesheet_directory() . '/functions/wm-autocomplete.php');
+include_once(get_stylesheet_directory() . '/functions/wm-breadcrumbs.php');
+include_once(get_stylesheet_directory() . '/functions/wm-contact.php');
+include_once(get_stylesheet_directory() . '/functions/wm-minify.php');
+include_once(get_stylesheet_directory() . '/functions/wp-clean.php');
+include_once(get_stylesheet_directory() . '/functions/wp-custom-post-type.php');
+include_once(get_stylesheet_directory() . '/functions/wp-custom-post-type-icons.php');
+include_once(get_stylesheet_directory() . '/functions/wp-hide-admin-items.php');
+include_once(get_stylesheet_directory() . '/functions/wp-image-sizes.php');
+include_once(get_stylesheet_directory() . '/functions/wp-options.php');
+include_once(get_stylesheet_directory() . '/functions/wp-widgets.php');
 
 
 /* Register menu's
