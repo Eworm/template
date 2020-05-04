5103c03d8836a2e8d3f9108c3bf07254bf992e7b
Wout <wout@woutmager.nl>
2020-05-04 10:05:22 +0200

    Bumping versions

--- js-src/base/lab-loader.js
+++ js-src/base/lab-loader.js
@@ -1,4 +1,4 @@
 const $L = $LAB
-    .script('//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js').wait()
+    .script('//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js').wait()
     // .script('//code.jquery.com/jquery-3.1.1.slim.min.js').wait()
     .script('/js/functions.min.js').wait();
